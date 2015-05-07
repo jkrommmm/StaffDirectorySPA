479e67abcc2f7fd26552a96a83f42848a4a9367a
jkrommmm <jacobkromm0@gmail.com>
2015-05-06 21:26:27 -0400

    Class Files set Up

---
+++ scripts/PersonClass.js
@@ -0,0 +1,21 @@
+/**
+ *   AUTHOR: Jacob Kromm jacobkromm0@gmail.com
+ *   VERSION: 1.0
+ *   CREATED: 05.04.2015
+ *   ASSIGNMENT: Staff Directory
+ *   TODO:
+ */
+
+"use strict";
+
+function PersonClass(building, lastName, firstName, job, subject, grade, phone, email) {
+	this.building = building;
+	this.lastName = lastName;
+	this.firstName = firstName;
+	this.job = job;
+	this.subject = subject;
+	this.grade = grade;
+	this.phone = phone;
+	this.email = email;
+}
+
